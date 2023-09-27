b8b9fda4173fa34a3d9947d316c4f7cb327889a8
Jaydip Nakarani <jaydip@moveeasy.com>
2023-09-27 17:06:43 +0530

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # django_examples
 Django Examples
+
+# jinja example url
+
+https://watch.screencastify.com/v/crRf2JJTLQbh3EtD6tB4
--- jinja_example/views.py
+++ jinja_example/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {'name': 'GreekForGreek'}  # Example context data
+    context = {'name': 'GreekForGreek Video recording'}  # Example context data
     return render(request, 'jinja_example.html', context)
